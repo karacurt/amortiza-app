e650105c66c7533957d987c6d2dd0185ab7591e2
Daniel Lima <daniel-lima@usp.br>
2021-10-22 02:40:47 -0300

    calcula price working

--- src/services/calculaPRICE.ts
+++ src/services/calculaPRICE.ts
@@ -1,7 +1,27 @@
-export function calculaPRICE(financing: number, fees: number, parcelas: number) {
-  const feesByMonth = fees / 12
-  console.log(financing, fees, parcelas)
-  const parcelaValue = financing * ((Math.pow(1 + feesByMonth, parcelas) * feesByMonth) / (Math.pow(1 + feesByMonth, parcelas) - 1))
-  console.log('parcelaValue')
-  console.log(parcelaValue)
+export function calculaPRICE(valorFinanciado: number, jurosAnual: number, parcelas: number) {
+  const jurosMensal = jurosAnual / 100 / 12
+  console.log(valorFinanciado, jurosAnual, jurosMensal, parcelas)
+
+  const pot = Math.pow(1 + jurosMensal, parcelas)
+
+  const valorParcela = valorFinanciado * ((pot * jurosMensal) / (pot - 1))
+
+  let parcelasPRICE = []
+  let saldoDevedor = valorFinanciado
+  for (let i = 0; i < parcelas; i++) {
+    const juros = saldoDevedor * jurosMensal
+    const amortizacao = valorParcela - juros
+    const parcela = {
+      id: i + 1,
+      numero: i + 1,
+      valor: `R$ ${valorParcela.toFixed(2)}`,
+      amortizacao: `R$ ${amortizacao.toFixed(2)}`,
+      saldoDevedor: `R$ ${saldoDevedor.toFixed(2)}`,
+      juros: `R$ ${juros.toFixed(2)}`
+    }
+    parcelasPRICE.push(parcela)
+    saldoDevedor = saldoDevedor - amortizacao
+  }
+
+  return parcelasPRICE
 }
